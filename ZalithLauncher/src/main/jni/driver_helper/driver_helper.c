5efa7c5129fb4ff27ad3a5ef97ab16e9c1a66887
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-02-21 15:02:19 +0800

    Update driver_helper.c

--- ZalithLauncher/src/main/jni/driver_helper/driver_helper.c
+++ ZalithLauncher/src/main/jni/driver_helper/driver_helper.c
@@ -1,5 +1,6 @@
 //
 // Created by Vera-Firefly on 17.01.2025.
+// Modified to load SwiftShader Vulkan driver (libvk_swiftshader.so)
 //
 #include <EGL/egl.h>
 #include <stdbool.h>
@@ -10,52 +11,9 @@
 #include "nsbypass.h"
 #include "GL/gl.h"
 
-//#define ADRENO_POSSIBLE
-#ifdef ADRENO_POSSIBLE
-
+// Always return true for SwiftShader (software rendering)
 bool checkAdrenoGraphics() {
-    EGLDisplay eglDisplay = eglGetDisplay(EGL_DEFAULT_DISPLAY);
-    if (eglDisplay == EGL_NO_DISPLAY || eglInitialize(eglDisplay, NULL, NULL) != EGL_TRUE) 
-        return false;
-
-    EGLint egl_attributes[] = {
-        EGL_BLUE_SIZE, 8, EGL_GREEN_SIZE, 8, EGL_RED_SIZE, 8,
-        EGL_ALPHA_SIZE, 8, EGL_DEPTH_SIZE, 24, EGL_SURFACE_TYPE, EGL_PBUFFER_BIT,
-        EGL_RENDERABLE_TYPE, EGL_OPENGL_ES2_BIT, EGL_NONE
-    };
-
-    EGLint num_configs = 0;
-    if (eglChooseConfig(eglDisplay, egl_attributes, NULL, 0, &num_configs) != EGL_TRUE || num_configs == 0) {
-        eglTerminate(eglDisplay);
-        return false;
-    }
-
-    EGLConfig eglConfig;
-    eglChooseConfig(eglDisplay, egl_attributes, &eglConfig, 1, &num_configs);
-
-    const EGLint egl_context_attributes[] = { EGL_CONTEXT_CLIENT_VERSION, 3, EGL_NONE };
-    EGLContext context = eglCreateContext(eglDisplay, eglConfig, EGL_NO_CONTEXT, egl_context_attributes);
-    if (context == EGL_NO_CONTEXT) {
-        eglTerminate(eglDisplay);
-        return false;
-    }
-
-    if (eglMakeCurrent(eglDisplay, EGL_NO_SURFACE, EGL_NO_SURFACE, context) != EGL_TRUE) {
-        eglDestroyContext(eglDisplay, context);
-        eglTerminate(eglDisplay);
-        return false;
-    }
-
-    const char* vendor = (const char*)glGetString(GL_VENDOR);
-    const char* renderer = (const char*)glGetString(GL_RENDERER);
-
-    bool is_adreno = (vendor && renderer && strcmp(vendor, "Qualcomm") == 0 && strstr(renderer, "Adreno") != NULL);
-
-    eglMakeCurrent(eglDisplay, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT);
-    eglDestroyContext(eglDisplay, context);
-    eglTerminate(eglDisplay);
-
-    return is_adreno;
+    return true;
 }
 
 void* loadTurnipVulkan() {
@@ -75,8 +33,9 @@ void* loadTurnipVulkan() {
     if (!linkerhook)
         return NULL;
 
-    void* turnip_driver_handle = linker_ns_dlopen("libvulkan_freedreno.so", RTLD_LOCAL | RTLD_NOW);
-    if (!turnip_driver_handle) {
+    // Load SwiftShader driver instead of Turnip
+    void* swiftshader_driver_handle = linker_ns_dlopen("libvk_swiftshader.so", RTLD_LOCAL | RTLD_NOW);
+    if (!swiftshader_driver_handle) {
         dlclose(linkerhook);
         return NULL;
     }
@@ -84,7 +43,7 @@ void* loadTurnipVulkan() {
     void* dl_android = linker_ns_dlopen("libdl_android.so", RTLD_LOCAL | RTLD_LAZY);
     if (!dl_android) {
         dlclose(linkerhook);
-        dlclose(turnip_driver_handle);
+        dlclose(swiftshader_driver_handle);
         return NULL;
     }
 
@@ -94,21 +53,20 @@ void* loadTurnipVulkan() {
     if (!linkerhookPassHandles || !android_get_exported_namespace) {
         dlclose(dl_android);
         dlclose(linkerhook);
-        dlclose(turnip_driver_handle);
+        dlclose(swiftshader_driver_handle);
         return NULL;
     }
 
-    linkerhookPassHandles(turnip_driver_handle, android_dlopen_ext, android_get_exported_namespace);
+    // Pass the SwiftShader handle to the hook
+    linkerhookPassHandles(swiftshader_driver_handle, android_dlopen_ext, android_get_exported_namespace);
 
     void* libvulkan = linker_ns_dlopen_unique(cache_dir, "libvulkan.so", RTLD_LOCAL | RTLD_NOW);
     if (!libvulkan) {
         dlclose(dl_android);
         dlclose(linkerhook);
-        dlclose(turnip_driver_handle);
+        dlclose(swiftshader_driver_handle);
         return NULL;
     }
 
     return libvulkan;
 }
-
-#endif
